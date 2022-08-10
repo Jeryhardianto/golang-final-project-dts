ea5f83f752e853be893c968d1816095f9058401f
Jeryhardianto <hardiantojr292gmail.com>
2022-08-10 09:03:30 +0700

    golang deploy

--- app/database.go
+++ app/database.go
@@ -7,7 +7,9 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:123456789@tcp(localhost:3306)/golang_tugas")
+	//db, err := sql.Open("mysql", "root:123456789@tcp(localhost:3306)/golang_tugas")
+	//Host MySQL HeroKu
+	db, err := sql.Open("mysql", "bbd3a79635f3f8:eb4c44e6@tcp(us-cdbr-east-06.cleardb.net:3306)/golang_tugas")
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
