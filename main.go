d3a96cb6ace184e3e75075ec434d74c917cd4ac2
Jeryhardianto <hardiantojr292gmail.com>
2022-08-10 10:05:06 +0700

    golang deploy

--- main.go
+++ main.go
@@ -4,18 +4,22 @@ import (
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
+	"io"
 	"jeryhardianto/golang-tugas/app"
 	"jeryhardianto/golang-tugas/controller"
 	"jeryhardianto/golang-tugas/helper"
 	"jeryhardianto/golang-tugas/repository"
 	"jeryhardianto/golang-tugas/service"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
 	db := app.NewDB()
 	validate := validator.New()
+	//server := gin.Default()
 
 	taskRepository := repository.NewTaskRepository()
 	taskService := service.NewTaskService(taskRepository, db, validate)
@@ -28,14 +32,24 @@ func main() {
 	router.POST("/api/tasks", taskController.Create)
 	router.PUT("/api/tasks/:taskId", taskController.Update)
 	router.PATCH("/api/tasks/:taskId", taskController.DoneTask)
+	port := os.Getenv("PORT")
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+	http.HandleFunc("/", helloHandler)
+	log.Println("Listing for" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
 
 	server := http.Server{
 		//Addr:    "localhost:3000",
-		Addr:    "0.0.0.0:3000",
+		Addr:    "0.0.0.0",
 		Handler: router,
 	}
 
 	err := server.ListenAndServe()
+
 	helper.PanicIfError(err)
 
 }
